1b1118734c1fd813bb5eda48b32189eeca4d275e
Dmitry <dmitry27zh@gmail.com>
2024-03-23 17:36:46 +0500

    Create Button

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import cls from './ThemeSwitcher.module.scss'
 import { Theme, useTheme } from 'app/providers/ThemeProvider'
 import LightIcon from 'shared/assets/icons/theme-light.svg'
 import DarkIcon from 'shared/assets/icons/theme-dark.svg'
+import { Button, ThemeButton } from 'shared/ui/Button/Button'
 
 interface ThemeSwitcherProps {
   className?: string
@@ -12,8 +13,8 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <button className={classNames(cls.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
+    <Button className={classNames(cls.ThemeSwitcher, {}, [className])} theme={ThemeButton.CLEAR} onClick={toggleTheme}>
       {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
-    </button>
+    </Button>
   )
 }
