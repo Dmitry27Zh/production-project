c6d78f06ac2e3e4d904fdd442cad68da45a948d3
Dmitry <dmitry27zh@gmail.com>
2024-03-25 12:45:14 +0500

    Apply i18n Ally support

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
 import cls from './NavBar.module.scss'
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
 
 interface NavBarProps {
   className?: string
