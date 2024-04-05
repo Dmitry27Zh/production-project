89ea40d158ed17a54d8628f51d78c0ce99c9c584
Dmitry <dmitry27zh@gmail.com>
2024-04-05 16:20:34 +0500

    Apply style decorator

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button, ThemeButton } from './Button';
-import 'app/styles/index.scss';
 
 const meta: Meta<typeof Button> = {
   title: 'shared/Button',
