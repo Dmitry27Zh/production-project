1df6f098a20905fe18d9ff3e47e6c0b1c1dda710
Dmitry <dmitry27zh@gmail.com>
2024-04-16 17:12:52 +0500

    Add sign in Modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,17 @@
-/* eslint-disable i18next/no-literal-string */
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router/';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
   const { theme } = useTheme();
-  const [isModalOpen, setIsModalOpen] = useState(false);
   document.body.setAttribute('data-theme', theme);
 
   return (
     <div className="app">
       <Suspense fallback="">
         <Navbar />
-        <button onClick={() => setIsModalOpen((prev) => !prev)} type="button">Toggle</button>
-        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-          Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Est tenetur amet, quam repudiandae culpa
-          minima tempore repellat
-          error earum
-          eligendi quod at quisquam odio porro! Consequuntur optio mollitia debitis dignissimos.
-        </Modal>
         <div className="content-page">
           <Sidebar />
           <AppRouter />
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useCallback, useState } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,12 +11,21 @@ interface NavbarProps {
 
 export function Navbar({ className }: NavbarProps) {
   const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
 
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links}>
-        /
-      </div>
+      <Button theme={ButtonTheme.CLEAR_INVERTED} className={cls.links} onClick={onToggleModal}>
+        {t('Log In')}
+      </Button>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatem, laboriosam.
+      </Modal>
     </div>
   );
 }
