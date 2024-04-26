8732304c0ffc8b0c1f7f7e26b7442962bd2cbeee
Dmitry <dmitry27zh@gmail.com>
2024-04-26 16:32:20 +0500

    Extract DynamicModuleLoader

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,39 +1,34 @@
-import React, { useCallback, useEffect } from 'react';
-import { useDispatch, useSelector, useStore } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Input } from 'shared/ui/Input/Input';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
-import { ReduxStoreWithManager } from 'app/providers/StoreProvider';
+import { loginReducer } from 'features/AuthByUsername';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { getLoginUsername } from '../../model/selectors/getLoginUsername/getLoginUsername';
 import { getLoginPassword } from '../../model/selectors/getLoginPassword/getLoginPassword';
 import { getLoginLoading } from '../../model/selectors/getLoginLoading/getLoginLoading';
 import { getLoginError } from '../../model/selectors/getLoginError/getLoginError';
 import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
-import { loginActions, loginReducer } from '../../model/slice/loginSlice';
+import { loginActions } from '../../model/slice/loginSlice';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
   className?: string
 }
 
+const initialReducers: ReducersList = { login: loginReducer };
+
 const LoginForm = React.memo(({ className }: LoginFormProps) => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const store = useStore() as ReduxStoreWithManager;
   const username = useSelector(getLoginUsername);
   const password = useSelector(getLoginPassword);
   const isLoading = useSelector(getLoginLoading);
   const error = useSelector(getLoginError);
-  useEffect(() => {
-    store.reducerManager.add('login', loginReducer);
 
-    return () => {
-      store.reducerManager.remove('login');
-    };
-    // eslint-disable-next-line
-  }, []);
   const onChangeUsername = useCallback((value: string) => {
     dispatch(loginActions.setUsername(value));
   }, [dispatch]);
@@ -46,33 +41,35 @@ const LoginForm = React.memo(({ className }: LoginFormProps) => {
   }, [dispatch, username, password]);
 
   return (
-    <div className={classNames(cls.LoginForm, {}, [className])}>
-      <Text title={t('Log in form')} />
-      {error && <Text text={t('Wrong login or password')} theme={TextTheme.ERROR} />}
-      <Input
-        type="text"
-        className={cls.input}
-        placeholder={t('Enter username')}
-        autoFocus
-        onChange={onChangeUsername}
-        value={username}
-      />
-      <Input
-        type="text"
-        className={cls.input}
-        placeholder={t('Enter password')}
-        onChange={onChangePassword}
-        value={password}
-      />
-      <Button
-        theme={ButtonTheme.OUTLINE}
-        className={cls.loginBtn}
-        onClick={onLoginClick}
-        disabled={isLoading}
-      >
-        {t('Sign In')}
-      </Button>
-    </div>
+    <DynamicModuleLoader reducers={initialReducers} removeAfterUnmount>
+      <div className={classNames(cls.LoginForm, {}, [className])}>
+        <Text title={t('Log in form')} />
+        {error && <Text text={t('Wrong login or password')} theme={TextTheme.ERROR} />}
+        <Input
+          type="text"
+          className={cls.input}
+          placeholder={t('Enter username')}
+          autoFocus
+          onChange={onChangeUsername}
+          value={username}
+        />
+        <Input
+          type="text"
+          className={cls.input}
+          placeholder={t('Enter password')}
+          onChange={onChangePassword}
+          value={password}
+        />
+        <Button
+          theme={ButtonTheme.OUTLINE}
+          className={cls.loginBtn}
+          onClick={onLoginClick}
+          disabled={isLoading}
+        >
+          {t('Sign In')}
+        </Button>
+      </div>
+    </DynamicModuleLoader>
   );
 });
 
---
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren, useEffect } from 'react';
+import { ReduxStoreWithManager } from 'app/providers/StoreProvider';
+import { useDispatch, useStore } from 'react-redux';
+import { StateSchemaKey } from 'app/providers/StoreProvider/config/StateSchema';
+import { Reducer } from '@reduxjs/toolkit';
+
+export type ReducersList = {
+  [name in StateSchemaKey]?: Reducer
+};
+
+export type ReducersListEntry = [StateSchemaKey, Reducer];
+
+interface DynamicModuleLoaderProps extends PropsWithChildren {
+  reducers: ReducersList
+  removeAfterUnmount?: boolean
+}
+
+export function DynamicModuleLoader({
+  children, reducers, removeAfterUnmount,
+}: DynamicModuleLoaderProps) {
+  const store = useStore() as ReduxStoreWithManager;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    Object.entries(reducers).forEach(([name, reducer]: ReducersListEntry) => {
+      store.reducerManager.add(name, reducer);
+      dispatch({ type: `@INIT ${name} reducer` });
+    });
+
+    return () => {
+      if (removeAfterUnmount) {
+        Object.entries(reducers).forEach(([name]: ReducersListEntry) => {
+          store.reducerManager.remove(name);
+          dispatch({ type: `@DESTROY ${name} reducer` });
+        });
+      }
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  return children;
+}
